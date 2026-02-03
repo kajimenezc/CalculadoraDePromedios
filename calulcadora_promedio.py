735510fd77b6be3f2e2404f29a7d1ceecaad7694
Kevin J <kevin.jimenezc97@gmail.com>
2026-02-02 23:02:14 -0500

    Add: function for find average, upper and lower values by subject

---
+++ calulcadora_promedio.py
@@ -0,0 +1,127 @@
+
+
+def ingresar_calificaciones():
+
+    nombres = []
+    calificaciones = []
+    isAddSubject = True
+
+    while isAddSubject:
+
+        while True:
+            nombre = input("Ingrese el nombre de la materia: ")
+            if len(nombre) == 0:
+                print("El nombre de la materia no puede estar vacio")
+            elif len(nombre) > 30:
+                print("El nombre de la materia no puede tener mas de 30 caracteres")
+            else:
+                nombres.append(nombre)
+                break
+        
+        while  True:
+            try:
+                calificacion = float(input("Ingrese una calificacion de 1 a 10: "))
+                if calificacion >= 1 and calificacion <= 10:
+                    calificaciones.append(calificacion)
+                    break
+                else:
+                    print("La calificación debe ser mayor a 1 y menor o igual a 10") 
+            except ValueError:
+                print("Debe ingresar un número válido. Ejemplo: 5.5")
+        
+        opcion = input("¿Desea agregar otra materia? (si/no): ")
+
+        while True:
+            if opcion.lower() != "si" and opcion.lower() != "no":
+                opcion = input("Opción inválida. Por favor ingrese 'si' o 'no': ")
+            else:
+                break
+
+        if opcion.lower() != "si":
+            isAddSubject = False   
+        
+    return nombres, calificaciones
+
+
+
+def calcular_promedio(calificaciones):
+    suma = sum(calificaciones)
+    promedio = suma / len(calificaciones) 
+    print("El promedio de las calificaciones es: ", promedio)
+    
+
+def determinar_estado(calificaciones, umbral, nombres):
+
+    aprobadas = []
+    reprobadas = []
+    count = 0
+
+    for calificacion in calificaciones:
+        
+        if calificacion >= umbral:
+            aprobadas.append(nombres[count])
+        else:
+            reprobadas.append(nombres[count])
+        count += 1
+    
+    print("Materias aprobadas:", ", ".join(aprobadas))
+    print("Materias reprobadas:", ", ".join(reprobadas))
+    
+    return aprobadas, reprobadas
+
+def encontrar_extremos(nombres, calificaciones):
+    count = 0
+    lower_index = 0
+    lower_value = 10
+    lower_name = ""
+    upper_index = 0
+    upper_value = 1
+    upper_name = ""
+
+    for calificacion in calificaciones:
+        
+        if calificacion <= lower_value:
+            lower_index = count
+            lower_value = calificacion
+            lower_name = nombres[count]
+
+        if calificacion > upper_value:
+            upper_index = count
+            upper_value = calificacion
+            upper_name = nombres[count]
+
+        count += 1
+
+    print(f"Mejor calificacion es {(upper_name)} con: ", upper_value)
+    print(F"Peor calificacion es {(lower_name)} con: ", lower_value)
+    
+    return lower_index, upper_index
+    
+
+
+def main():
+    print("Bienvenido a la calculadora de promedios")
+    nombres = []
+    calificaciones = []
+
+    nombres, calificaciones = ingresar_calificaciones()
+
+    for nombre, calificacione in zip(nombres, calificaciones):
+        print(f"Materia: {nombre}, Calificación: {calificacione}")
+
+    print(f"Total Materias: {len(calificaciones)}")
+    calcular_promedio(calificaciones)
+    determinar_estado(calificaciones, 5, nombres)
+    encontrar_extremos(nombres, calificaciones)
+
+    print("Te agradecemos por usar la calculadora de promedios")
+
+if __name__ == "__main__":
+    main()
+
+
+ 
+    
+
+
+
